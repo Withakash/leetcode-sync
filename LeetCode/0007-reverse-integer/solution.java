464e6bdd8ef0dcdf8e44a767ae1c0062305cd834
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2023-04-11 16:54:00 +0000

    [LeetCode Sync] Runtime - 1 ms (99.98%), Memory - 39.7 MB (100.00%)

---
+++ LeetCode/0007-reverse-integer/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        boolean isnegative=false;
+        if(x<0){
+            isnegative=true;
+            x=-x;
+        }
+        long reverse = 0;
+        while(x>0)
+        {
+            reverse = reverse * 10 + x % 10;
+            x=x/10;
+        }
+        if(reverse > Integer.MAX_VALUE){
+              return 0;
+        }
+      
+
+        return (int)(isnegative ? -reverse : reverse);
+    }
+}
