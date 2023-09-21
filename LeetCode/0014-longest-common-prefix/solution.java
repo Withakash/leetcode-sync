4e3bde10819693174491c973b95c33aec7c650c0
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2023-09-21 08:24:27 +0000

    [LeetCode Sync] Runtime - 8 ms (5.57%), Memory - 41.3 MB (99.99%)

--- LeetCode/0014-longest-common-prefix/solution.java
+++ LeetCode/0014-longest-common-prefix/solution.java
@@ -1,17 +1,23 @@
-public class Solution {
+class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
-            return "";
+
+        Arrays.sort(strs);
+        int size = strs.length;
+        int l = strs[0].length();
+        String pre = strs[0];
+        if(size==0) return "";
+        if(size==1) return pre;
+        String str = "";
+        char cc;
+      out:  for(int i = 0 ; i < l ; i++)
+          {
+           cc = pre.charAt(i);
+            for(int j = 1 ; j < strs.length && i < strs[j].length() ; j++)
+                if(cc != strs[j].charAt(i))
+                     break out;  
+             str+=cc;
         }
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i ++ ) {
-            for (int j = 0; j < prefix.length(); j ++){
-                if (j == strs[i].length() || prefix.charAt(j) != strs[i].charAt(j)) {
-                    prefix = prefix.substring(0, j);
-                    break;
-                }
-            }
-        }
-        return prefix;
+
+        return str;
     }
 }
