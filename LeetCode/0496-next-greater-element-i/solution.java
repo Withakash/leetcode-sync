a10bae21e516cc5343ba3cd316483bb5b498ad4f
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2025-12-17 03:45:54 +0000

    [LeetCode Sync] Runtime - 4 ms (74.44%), Memory - 45.4 MB (22.08%)

--- LeetCode/0496-next-greater-element-i/solution.java
+++ LeetCode/0496-next-greater-element-i/solution.java
@@ -1,30 +1,34 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n = nums2.length;
+    public int[] nextGreaterElement(int[] nums1, int[] arr) {
+        int n = arr.length;
         int[] nge = new int[n];
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-        Stack<Integer> stack = new Stack<>();
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-                stack.pop();
+        for(int i = n-1; i>=0 ; i--)
+        {
+            while(!st.isEmpty() && st.peek() <= arr[i])
+            {
+                st.pop();
             }
 
-            nge[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums2[i]);
+            if(st.isEmpty()) nge[i] =-1;
+            else nge[i] = st.peek();
+
+            st.push(arr[i]);
         }
 
-        for (int i = 0; i < n; i++) {
-            map.put(nums2[i], nge[i]);
+        for(int i =0 ; i < arr.length ; i++)
+        {
+            map.put(arr[i] , nge[i]);
         }
 
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = map.get(nums1[i]);  // SAFE now
+        int[] res = new int[nums1.length];
+        for(int i = 0 ; i<nums1.length ;i++)
+        {
+            res[i] = map.get(nums1[i]);
         }
-
-        return result;
+        
+        return res ;
     }
 }
