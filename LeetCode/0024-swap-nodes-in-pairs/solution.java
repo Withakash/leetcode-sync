c400331d25e06df6f46279eefb2782c00caadb38
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-01-05 02:15:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (47.26%)

--- LeetCode/0024-swap-nodes-in-pairs/solution.java
+++ LeetCode/0024-swap-nodes-in-pairs/solution.java
@@ -1,27 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-  public ListNode swapPairs(ListNode head) {
-    final int length = getLength(head);
-    ListNode dummy = new ListNode(0, head);
-    ListNode prev = dummy;
-    ListNode curr = head;
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode point = dummy;
 
-    for (int i = 0; i < length / 2; ++i) {
-      ListNode next = curr.next;
-      curr.next = next.next;
-      next.next = curr;
-      prev.next = next;
-      prev = curr;
-      curr = curr.next;
+        while(point.next != null && point.next.next != null) {
+            ListNode node1 = point.next;
+            ListNode node2 = point.next.next;
+
+            node1.next = node2.next;
+            node2.next = node1;
+
+            point.next = node2;
+            point = node1;
+
+        }
+         return dummy.next;
     }
-
-    return dummy.next;
-  }
-
-  private int getLength(ListNode head) {
-    int length = 0;
-    for (ListNode curr = head; curr != null; curr = curr.next)
-      ++length;
-    return length;
-  }
 }
-
