a9f6fc2e3a2962fa0a71f24fc3e0a70d179fc760
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-01-06 06:59:47 +0000

    [LeetCode Sync] Runtime - 11 ms (93.93%), Memory - 47.3 MB (18.79%)

---
+++ LeetCode/0636-exclusive-time-of-functions/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prevTime = 0;
+
+        for (String log : logs) {
+
+            String[] parts = log.split(":");
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int time = Integer.parseInt(parts[2]);
+
+            // START log
+            if (type.equals("start")) {
+
+                if (!stack.isEmpty()) {
+                    result[stack.peek()] += time - prevTime;
+                }
+
+                stack.push(id);
+                prevTime = time;
+            }
+
+            // END log
+            else {
+                result[stack.pop()] += time - prevTime + 1;
+                prevTime = time + 1;
+            }
+        }
+
+        return result;
+    }
+}
+
