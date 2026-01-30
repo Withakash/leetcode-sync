11bcc75ddf8617ab6e4863fd6176d0756f5b24d8
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-01-30 05:24:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.2 MB (50.49%)

--- LeetCode/0144-binary-tree-preorder-traversal/solution.java
+++ LeetCode/0144-binary-tree-preorder-traversal/solution.java
@@ -14,14 +14,17 @@
  * }
  */
 class Solution {
-    List<Integer> li = new ArrayList<Integer>();
-    public List<Integer> preorderTraversal(TreeNode root) {
-        if(root == null) return li;
 
-        li.add(root.val);
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        if(root == null) return res;
+
+        res.add(root.val);
         preorderTraversal(root.left);
         preorderTraversal(root.right);
 
-        return li;
+        return res;
+        
     }
 }
