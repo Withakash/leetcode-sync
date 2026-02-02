2b6e08d1ed0e3585c155d9daa0988bb6b0caf374
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-02-02 18:09:23 +0000

    [LeetCode Sync] Runtime - 1 ms (10.28%), Memory - 44.8 MB (12.40%)

--- LeetCode/0104-maximum-depth-of-binary-tree/solution.java
+++ LeetCode/0104-maximum-depth-of-binary-tree/solution.java
@@ -15,7 +15,28 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+
+        if (root == null) return 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        int depth = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();  // number of nodes in current level
+            depth++;              // completed one level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+
+                if (curr.left != null)
+                    q.add(curr.left);
+
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+        return depth;
     }
 }
