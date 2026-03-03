b9343426824b780aaa0724a5b2a643b2d56f5d5d
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-03-03 02:07:07 +0000

    [LeetCode Sync] Runtime - 1 ms (9.72%), Memory - 43.7 MB (32.78%)

--- LeetCode/0101-symmetric-tree/solution.java
+++ LeetCode/0101-symmetric-tree/solution.java
@@ -15,15 +15,26 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        return sameTree(root.left , root.right);
-    }
-    
-    public boolean sameTree(TreeNode l,TreeNode r)
-    {
-        if(l == null && r == null) return true;
-        if(l == null || r == null) return false;
-        if(l.val != r.val) return false;
-        return sameTree(l.left,r.right) && sameTree(r.left,l.right);
+        if (root == null) return true;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()) {
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
+
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+
+            queue.add(t1.left);
+            queue.add(t2.right);
+            queue.add(t1.right);
+            queue.add(t2.left);
+        }
+
+        return true;
     }
 }
