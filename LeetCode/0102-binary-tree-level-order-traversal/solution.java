1a08ff77ed066f82954fa2c33e7155b394489886
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-03-27 08:01:20 +0000

    [LeetCode Sync] Runtime - 1 ms (96.18%), Memory - 46.4 MB (97.11%)

--- LeetCode/0102-binary-tree-level-order-traversal/solution.java
+++ LeetCode/0102-binary-tree-level-order-traversal/solution.java
@@ -16,24 +16,26 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
-       if(root == null) return res;
+        if(root == null) return res;
 
-       Queue<TreeNode> q = new LinkedList<>();
-       q.add(root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
 
-       while(!q.isEmpty()) {
-        int size = q.size();
-        List<Integer> level = new ArrayList<>();
+        while(!q.isEmpty()) {
 
-        for(int i =0 ; i < size ; i++) {
-        TreeNode curr = q.poll();
-        level.add(curr.val);
-        if(curr.left != null) q.add(curr.left);
-        if(curr.right != null) q.add(curr.right);
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+           for( int i = 0 ; i < size ; i++) {
+              TreeNode curr = q.poll();
+              level.add(curr.val);
+              if(curr.left != null) q.add(curr.left);
+              if(curr.right != null) q.add(curr.right);
+           }
+
+           res.add(level);
+
         }
-         res.add(level);
-       } 
-
-       return res;
+        return res;
     }
 }
