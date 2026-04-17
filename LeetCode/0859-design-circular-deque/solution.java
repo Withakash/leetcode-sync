d731e58847c9adb2b70c36e7ab053f5e82402b9f
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-04-17 07:19:20 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 46.5 MB (49.42%)

--- LeetCode/0859-design-circular-deque/solution.java
+++ LeetCode/0859-design-circular-deque/solution.java
@@ -1,86 +1,93 @@
 class MyCircularDeque {
-
-    int capacity;
     int[] arr;
-    int size;
-    int front;
-    int rear;
+    int size, capacity, front, rear;
 
     public MyCircularDeque(int k) {
         capacity = k;
-        arr = new int[capacity];
         size = 0;
         front = 0;
         rear = -1;
+
+        arr = new int[capacity];
     }
 
     public boolean insertFront(int value) {
-      if(size == capacity) return false;
-      
-      if(isEmpty()) 
-        front = rear = 0;
-       else
-      front = ( front - 1 + capacity) % capacity;
+        if (isFull())
+            return false;
 
-      arr[front] = value;
-      size++;
-      return true;
+        if (isEmpty()) {
+            front = 0;
+            rear = 0;
+        }
+        else 
+        front = (front - 1 + capacity) % capacity;
+        arr[front] = value;
+        size++;
+        return true;
 
     }
 
-    public boolean insertLast(int value) { 
-        if(size == capacity) return false;
-        if(isEmpty())
-         front  = rear = 0;
-        else 
+    public boolean insertLast(int value) {
+        if (isFull())
+            return false;
+            
+
         rear = (rear + 1) % capacity;
         arr[rear] = value;
         size++;
         return true;
+
     }
 
     public boolean deleteFront() {
-       if(size == 0) return  false;
 
-       front = (front + 1) % capacity; 
-       size--;
+        if (isEmpty())
+            return false;
+        front = (front + 1) % capacity;
+        size--;
 
-       if(size == 0){
-        front = 0;
-        rear = -1;
-       }
-       return true;
+        if (size == 0) {
+            front = 0;
+            rear = -1;
+        }
+        return true;
+
     }
 
     public boolean deleteLast() {
-       if(size == 0) return false;
+        if (isEmpty())
+            return false;
 
-       rear = (rear - 1 + capacity) % capacity;
-       size--;
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+        if (size == 0) {
+            front = 0;
+            rear = -1;
+        }
+        return true;
 
-       if(size == 0) {
-        front = 0;
-        rear = -1;
-       }
-       return true;
     }
 
     public int getFront() {
-       if(size == 0) return -1;
-       return arr[front];
+
+        if (isEmpty())
+            return -1;
+        return arr[front];
     }
 
     public int getRear() {
-       if(size == 0 || rear == -1) return -1;
-       return arr[rear];
+
+        if (isEmpty() || rear == -1)
+            return -1;
+        return arr[rear];
     }
 
     public boolean isEmpty() {
-       return size == 0;
+        return size == 0;
     }
 
     public boolean isFull() {
-       return size == capacity;
+        return size == capacity;
     }
 }
 
