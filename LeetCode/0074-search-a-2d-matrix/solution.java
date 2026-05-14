feebf9a11ed4ec865a282dc8864a6d51c2a2f59b
Akash Yadav <110447309+Withakash@users.noreply.github.com>
2026-05-14 16:31:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (23.68%)

--- LeetCode/0074-search-a-2d-matrix/solution.java
+++ LeetCode/0074-search-a-2d-matrix/solution.java
@@ -1,27 +1,13 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    int n = matrix.length;
-    int m = matrix[0].length;
-    List<Integer> al = new ArrayList<Integer>();
-    int k=0;
-    if(matrix[0][0]==target) return true;
-    for(int i = 0 ; i < n ; i++ )
-    {
-        for(int j = 0 ; j < m ; j++)
-        {
-              al.add(matrix[i][j]);
+        int rSize = matrix.length;
+        int cSize = matrix[0].length;
+
+        for(int i = 0 ; i < rSize ; i++){
+            for(int j = 0 ; j < cSize ; j++){
+                if(matrix[i][j] == target) return true;
+            }
         }
-    }
-
-    int result = Collections.binarySearch(al, target);
-       
-        if (result <= 0)
-            return false;
-        else
-            return true;
-        // System.out.println(result);
-        // return true;
-
+        return false;
     }
 }
-
